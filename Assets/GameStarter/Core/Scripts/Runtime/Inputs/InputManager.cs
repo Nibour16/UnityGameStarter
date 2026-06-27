e0c90bafc7e64195fbdaaf13717bbed0b18aa4bb
Nibour16 <ruobinh3@hotmail.com>
2026-06-26 18:03:41 -0600

    Standard Input Manager

---
+++ Assets/GameStarter/Core/Scripts/Runtime/Inputs/InputManager.cs
@@ -0,0 +1,26 @@
+namespace UnityGameStarter.InputSystem 
+{
+    using UnityGameStarter.SingletonPattern;
+
+    public abstract class InputManager : Singleton<InputManager>
+    {
+        private PlayerInputs_Core _core;
+        public PlayerInputs_Core Core => _core;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _core = new PlayerInputs_Core();
+        }
+
+        protected virtual void OnEnable() 
+        {
+            _core?.Enable();
+        }
+
+        protected virtual void OnDisable() 
+        {
+            _core?.Disable();
+        }
+    }
+}
--- Assets/GameStarter/Core/Scripts/Utilities/Singleton/Singleton.cs
+++ Assets/GameStarter/Core/Scripts/Utilities/Singleton/Singleton.cs
@@ -5,33 +5,24 @@ namespace UnityGameStarter.SingletonPattern
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         private static T _instance;
-
-        public static T Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = FindAnyObjectByType<T>(); //Find any active object in the scene that has the specific script type
-
-                    if (_instance == null)  //if cannot find it
-                        Debug.LogError($"{typeof(T)} is not found in the scene!");
-                }
-                return _instance;
-            }
-        }
+        public static T Instance => _instance;
 
         protected virtual void Awake()
         {
-            if (_instance == null)
-            {
-                _instance = this as T;
-                DontDestroyOnLoad(gameObject);
-            }
-            else if (_instance != this)
+            if (_instance != null && _instance != this)
             {
                 Destroy(gameObject);
+                return;
             }
+
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (_instance == this)
+                _instance = null;
         }
     }
 }
