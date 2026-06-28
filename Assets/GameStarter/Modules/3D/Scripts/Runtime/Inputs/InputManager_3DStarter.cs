1dd75ac9d65a4c1503db65ccf5ecf9f77e62747c
Nibour16 <ruobinh3@hotmail.com>
2026-06-27 21:01:57 -0600

    Starter Input Managers

---
+++ Assets/GameStarter/Modules/3D/Scripts/Runtime/Inputs/InputManager_3DStarter.cs
@@ -0,0 +1,26 @@
+namespace UnityGameStarter.InputSystem.Player_3D 
+{
+    public class InputManager_3DStarter : InputManager<InputManager_3DStarter>
+    {
+        private PlayerInputs_3D_Starter _playerInputs;
+        public PlayerInputs_3D_Starter PlayerInputs => _playerInputs;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _playerInputs = new PlayerInputs_3D_Starter();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _playerInputs?.Enable();
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            _playerInputs?.Disable();
+        }
+    }
+}
