f983cd6962bf5ba796c64782703d249cf3e82c6a
Nibour16 <ruobinh3@hotmail.com>
2026-07-16 17:03:52 -0600

    Game Manager

--- Assets/GameStarter/Gameplay/Scripts/Runtime/GameplayManagement/GameManager.cs
+++ Assets/GameStarter/Gameplay/Scripts/Runtime/GameplayManagement/GameManager.cs
@@ -1,10 +1,54 @@
 using UnityEngine;
 using UnityGameStarter.SingletonPattern;
 
-namespace UnityGameStarter.Gameplay.Management 
+namespace UnityGameStarter.Gameplay.Core
 {
     public class GameManager : Singleton<GameManager>
     {
+        public GameplayData data;
         
+        [SerializeField] private GameStateMachine stateMachine;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (!stateMachine) stateMachine = FindAnyObjectByType<GameStateMachine>();
+
+            if (!stateMachine) 
+                Debug.LogError(
+                    "Game Manager: Cannot find Game State Machine in the scene." +
+                    "Please assign it in the inspector or ensure one exists in the scene.");
+        }
+
+        public void SaveGame() 
+        {
+            // TODO: Save the game by using Save Manager
+        }
+        
+        public void LoadGame() 
+        {
+            stateMachine.SetState(typeof(LoadingState));
+        }
+        
+        public void PauseGame() 
+        {
+            stateMachine.SetState(typeof(PauseState));
+        }
+
+        public void ResumeGame() 
+        {
+            stateMachine.SetState(typeof(GameplayState));
+        }
+
+        public void RestartGame() 
+        {
+            // TODO: restart the game
+        }
+
+        public void LeaveGame() 
+        {
+            // TODO: Quit Game
+        }
     }
 }
